64f4b6bec6103fafa072cbfcf3589821d39eda31
Scott Schaefer <scottschaef@gmail.com>
2020-06-10 21:16:46 -0400

    more card

--- src/components/shared/Card.vue
+++ src/components/shared/Card.vue
@@ -1,8 +1,5 @@
 <template>
-  <div
-    class="shadow-md w-64 h-64 border border-solid border-gray-100"
-    :class="getSize"
-  >
+  <div class="shadow-md border border-solid border-gray-100" :class="getSize">
     <slot />
   </div>
 </template>
@@ -13,20 +10,6 @@ export default {
     size: {
       type: String,
       default: "medium"
-    }
-  },
-
-  computed: {
-    getSize() {
-      return "";
-      // switch (this.size) {
-      //   case 'small':
-      //     return { 'h-64'}
-      //     break;
-
-      //   default:
-      //     break;
-      // }
     }
   }
 };
