db1ca8402026c90f2cf496e6b712f9caec7ad98b
Scott Schaefer <scottschaef@gmail.com>
2020-11-09 19:54:24 -0500

    Create logout functionality (#7)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="flex justify-between border-b border-black border-solid">
     <img class="w-40" src="http://pigment.github.io/fake-logos/logos/small/color/crofts-accountants.png" />
-    <img src="../assets/icons/account_circle-24px.svg" />
+    <img @click="logout" src="../assets/icons/account_circle-24px.svg" />
   </div>
 </template>
+
+<script>
+import AuthService from '@/services/AuthService';
+
+export default {
+  methods: {
+    logout() {
+      AuthService.deleteToken();
+      this.$router.push('/login');
+    }
+  }
+};
+</script>
