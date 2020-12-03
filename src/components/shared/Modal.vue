c4b1a747284157f3ed9c9e49e74b6ca09b38ee1a
Scott Schaefer <scottschaef@gmail.com>
2020-12-02 21:01:51 -0500

    add modal box

--- src/components/shared/Modal.vue
+++ src/components/shared/Modal.vue
@@ -1,7 +1,9 @@
 <template>
   <portal to="modal">
-    <div class="h-screen w-screen fixed z-40 top-0 left-0 bg-opacity-25 bg-blue-900">
-      <slot />
+    <div class="h-screen w-screen fixed z-40 top-0 left-0 bg-opacity-25 bg-blue-900 ease-in">
+      <div class="w-64 h-64 p-8 bg-white border-gray-700 rounded-md shadow-md">
+        <slot />
+      </div>
     </div>
   </portal>
 </template>
