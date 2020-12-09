b13afb2019fda8e3d09edad38d07c5d35d7e5b85
Scott Schaefer <scottschaef@gmail.com>
2020-12-08 20:15:22 -0500

    clean up code

--- src/views/UserAdministration/EditUser.vue
+++ src/views/UserAdministration/EditUser.vue
@@ -59,7 +59,7 @@
         <button
           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="button"
-          @click.prevent="handleDelete"
+          @click="handleDelete"
         >
           Delete
         </button>
